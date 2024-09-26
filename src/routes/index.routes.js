c8f1963c5c43e921b1c48c79c62629da0bd2f762
gabriela-moleta <gabriela.moleta@aluno.senai.br>
2024-09-26 17:06:01 -0300

    feat: star to create characters routes

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import emocoesRoutes from "./emocoes.routes.js";
+import personagensRoutes from "./personagens.routes.js";
 
 const routes = Router();
 
@@ -8,5 +9,6 @@ routes.get("/2tds2",(req,res) => {
     });
 
     routes.use("/emocoes",emocoesRoutes);
+    routes.use("/personagens", personagensRoutes);
 
     export default routes;
--- src/routes/personagens.routes.js
+++ src/routes/personagens.routes.js
@@ -1,4 +1,8 @@
-const personagens = [
+import { Router } from "express"
+
+const personagensRoutes = Router ()
+
+let personagens = [
     {
     id: 100,
     nome: "Totoro",
@@ -25,6 +29,43 @@ const personagens = [
     }
     ]
 
-    app.get("/personagens",(req,res) => {
-        return res.status(200).send( personagens )
-        })
+   personagensRoutes.get("/",(req, res) => {
+    return res.status(200)
+    .send(personagens)
+   })
+
+   personagensRoutes.post("/",(req,res) => {
+    const { nome, vivo } = req.body;
+    
+    const novaPersonagem = {
+        id:personagens.length + 1,
+        nome: nome,
+        vivo: vivo,
+    };
+
+    personagens.push(novaPersonagem);
+    return res.status(201).send( novaPersonagem);
+    });
+
+    personagensRoutes.delete("/:id", (req, res) => {
+        const {id} = req.params;
+    
+        const personagens = personagens.filter((person) =>person.id == id );
+    
+        if (!personagens) {
+        return res.status(404).send({
+            message: "Persoangem nao encontrado",
+            personagens,
+        });
+        }
+    
+        personagens = personagens.filter((person) => person.id != id)
+    
+        return res.status(200).send({
+            message: "Emoção deletada",
+            personagens,
+        });
+    
+    });
+
+   export default personagensRoutes;
